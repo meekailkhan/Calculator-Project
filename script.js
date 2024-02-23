174bfc268d65285c44540748b177839571c7f3bf
meekailkhan <meekailkhan123aar@gmail.com>
2024-02-23 23:33:58 +0530

    calculator projet

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const devideBtn = tagGenrator("button","class","devideBtn",calculator,"/","/");
 const seventBtn = tagGenrator("button","class","seventBtn",calculator,"7","7");
 const eightBtn = tagGenrator("button","class","eightBtn",calculator,"8","8");
 const nineBtn = tagGenrator("button","class","nineBtn",calculator,"9","9");
-const plusBtn = tagGenrator("button","class","plusBtn",calculator,"+","+");
+const multiBtn = tagGenrator("button","class","multiBtn",calculator,"*","*");
 const fourBtn = tagGenrator("button","class","fourBtn",calculator,"4","4");
 const fiveBtn = tagGenrator("button","class","fiveBtn",calculator,"5","5");
 const sixBtn = tagGenrator("button","class","sixBtn",calculator,"6","6");
 const subBtn = tagGenrator("button","class","subBtn",calculator,"-","-");
-
+const oneBtn = tagGenrator("button","class","oneBtn",calculator,"1","1");
+const twoBtn = tagGenrator("button","class","twoBtn",calculator,"2","2");
+const threeBtn = tagGenrator("button","class","threeBtn",calculator,"3","3");
+const plusBtn = tagGenrator("button","class","plusBtn",calculator,"+","+");
+const zeroBtn = tagGenrator("button","class","zeroBtn",calculator,"0","0");
+const dZeroBtn = tagGenrator("button","class","dZeroBtn",calculator,"00","00");
+const doutBtn = tagGenrator("button","class","doutBtn",calculator,".",".")
 
 
 
